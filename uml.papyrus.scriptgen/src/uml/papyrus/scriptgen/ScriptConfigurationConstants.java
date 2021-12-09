18b7d63458b46dba3930bd069152498c6de40eea
Horváth Tibor <tibor.horvath@it4all.hu>
2021-12-09 21:16:42 +0100

    gitignore fix

--- uml.papyrus.scriptgen/src/uml/papyrus/scriptgen/ScriptConfigurationConstants.java
+++
@@ -1,8 +0,0 @@
-package uml.papyrus.scriptgen;
-
-public class ScriptConfigurationConstants {
-  public static final String PROJECT_NAME="projectName";
-  public static final String OUTPUT_FOLDER="outputFolder";
-  public static final String SQL_LENGTH_LIMIT="sqlLengthLimit";
-  public static final String LICENSE_GENERATION = "licenseGeneration";
-}
